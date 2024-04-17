b0097cdcc0230571bc131668c8272260243313d9
Caio Furtado de Miranda Carvalho <164534948+CaioFurtadoo@users.noreply.github.com>
2024-04-17 11:59:52 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function show() {
+    console.log("lol");
+    /*element = document.querySelector(".sideClose")
+    element.classList.toggle("show")
+    element2 = document.querySelector(".sideOpen")
+    element2.classList.toggle("open")*/
+
+    element = document.querySelector(".sideClose");
+    element.classList.toggle("test");
+}
